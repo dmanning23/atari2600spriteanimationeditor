9a904ddba1491f492a1aab36d635d15eb938c441
Dan Manning <1888089+dmanning23@users.noreply.github.com>
2024-12-05 21:20:37 -0600

    fix line color conmponent

--- components/LineColorSelector.js
+++ components/LineColorSelector.js
@@ -2,7 +2,6 @@
 'use client';
 
 import React from 'react';
-import colorPaletteData from '@/data/desaturated-color-palette.json';
 
 const LineColorSelector = ({ lineColors, onLineColorChange, getColorHex }) => {
     return (
