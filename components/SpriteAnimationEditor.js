107da16d301f9dac34b6c077d67916beec6f223c
Dan Manning <1888089+dmanning23@users.noreply.github.com>
2025-03-01 00:32:25 -0600

    fixed linter 2

--- components/SpriteAnimationEditor.js
+++ components/SpriteAnimationEditor.js
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useCallback, useRef } from 'react';
+import React, { useState, useCallback } from 'react';
 import colorPaletteData from '../data/desaturated-color-palette.json';
 
 // Import modular components - using relative paths
